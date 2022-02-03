fe32887716b213cabca5d398b7c9d3e15f569ec1
Ruairi-Grant <grantru@tcd.ie>
2022-02-03 21:10:21 +0000

    Fills requirements and tested

--- labs/lab01/lab01.c
+++ labs/lab01/lab01.c
@@ -1,27 +1,29 @@
 #include "pico/stdlib.h"
 
 /**
- * @brief EXAMPLE - BLINK_C
- *        Simple example to initialise the built-in LED on
+ * @brief Lab01
+ *        Simple program to initialise the built-in LED on
  *        the Raspberry Pi Pico and then flash it forever. 
  * 
  * @return int  Application return code (zero for success).
  */
 
+//state variable to track state of the LED
+bool g_state = false; 
 
-//Funcion to continuously blink an LED at specified pin and delay
-void blink_led(uint led_pin, uint led_delay){
-    while (true) {
+/**
+ * @brief toggles the state of the specified LED and waits for specified time
+ * 
+ * @param led_pin   selects which LED pin to toggle
+ * @param led_delay specifid time to wait after toggling
+ */
+void toggle_led(uint led_pin, uint led_delay){
 
-        // Toggle the LED on and then sleep for delay period
-        gpio_put(led_pin, 1);
+        // Toggle the LED then sleep for delay period
+        gpio_put(led_pin, !g_state);
         sleep_ms(led_delay);
-
-        // Toggle the LED off and then sleep for delay period
-        gpio_put(led_pin, 0);
-        sleep_ms(led_delay);
-
-    }
+        // Update the state of the LED
+        g_state = !g_state;
 }
 int main() {
 
@@ -33,9 +35,10 @@ int main() {
     gpio_init(LED_PIN);
     gpio_set_dir(LED_PIN, GPIO_OUT);
 
-    // blink the led at LED_PIN continuously at delay LED_DELAY
-     blink_led(LED_PIN, LED_DELAY);
-    
+    while (true){
+        // toggle the state of the LED
+        toggle_led(LED_PIN, LED_DELAY);
+    }
 
     // Should never get here due to infinite while-loop.
     return 0;
